48f4d6e19a18e5adaca004f1a1b40022788f61bf
Manny <malisauskas88@gmail.com>
2026-03-05 03:51:15 +0700

    Explicit no-cache for user profile

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+const fetchNoCache = (url: RequestInfo | URL, options?: RequestInit) =>
+  fetch(url, { ...options, cache: 'no-store' })
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  { global: { fetch: fetchNoCache } }
 )
