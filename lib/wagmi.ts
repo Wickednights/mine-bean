41cf52634ccd89a4b824c7a588bf3d37f353393d
nu11dotfun <nu11dotfun@users.noreply.github.com>
2026-03-07 00:17:55 +0400

    Add Phantom wallet support for mobile

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,11 +1,41 @@
 'use client'
 
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { connectorsForWallets } from '@rainbow-me/rainbowkit'
+import {
+  metaMaskWallet,
+  coinbaseWallet,
+  walletConnectWallet,
+  phantomWallet,
+  rainbowWallet,
+} from '@rainbow-me/rainbowkit/wallets'
+import { createConfig, http } from 'wagmi'
 import { base, baseSepolia } from 'wagmi/chains'
 
-export const config = getDefaultConfig({
-  appName: 'BEANS Protocol',
-  projectId: '666aa4c4e1ee459d4697a07bcf2f1ec6',
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Popular',
+      wallets: [
+        metaMaskWallet,
+        phantomWallet,
+        coinbaseWallet,
+        rainbowWallet,
+        walletConnectWallet,
+      ],
+    },
+  ],
+  {
+    appName: 'BEANS Protocol',
+    projectId: '666aa4c4e1ee459d4697a07bcf2f1ec6',
+  }
+)
+
+export const config = createConfig({
+  connectors,
   chains: [base, baseSepolia],
+  transports: {
+    [base.id]: http(),
+    [baseSepolia.id]: http(),
+  },
   ssr: true,
 })
